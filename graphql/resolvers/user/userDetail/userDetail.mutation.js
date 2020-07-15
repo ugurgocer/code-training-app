ff5b29db9686b716d1c0e763cd7ce429eb8cbbcc
Salih TEYEK <salih@teyek.com>
2020-07-16 00:43:32 +0300

    User Detail

--- graphql/resolvers/user/userDetail/userDetail.mutation.js
+++ graphql/resolvers/user/userDetail/userDetail.mutation.js
@@ -3,28 +3,34 @@ const { ValidationError } = require('./../../../../utils/errors/index')
 const { parseError, loadFromDataLoader, imageUpload } = require('./../../../../utils/helpers/other')
 const { regular } = require('../../../../utils/helpers/middleware')
 
-const setUserDetail = async(_, { user_id, user, userDetail }, { req, dataLoader }, info) => {
+const setUserDetail = async(_, { userId, user, userDetail }, { req, dataLoader }, info) => {
     regular(req)
-    
+    console.log("geldi")
     return db.sequelize.transaction(async trx => {
         try {
+            userDetail.userId = userId
+
             let image_id = null
             if(userDetail.profilePicture){
                 image_id = await imageUpload({ userId: req.account.id, ...userDetail.profilePicture}, trx)
                 delete userDetail.profilePicture
+                userDetail.imageId = image_id
             }
 
             if(user)
-                await db.User.update({ ...user }, { where: { id: user_id } }, {transaction: trx})
+                await db.User.update({ ...user }, { where: { id: userId } }, {transaction: trx})
 
-            const data = await db.UserDetail.findOne({ where: { user_id } })
+            const data = await db.UserDetail.findOne({ where: { userId } })
+
             if(data)
-                db.UserDetail.update({ ...userDetail, imageId: image_id, }, { where: { user_id } }, {transaction: trx} )
+                db.UserDetail.update(userDetail, { where: { userId } }, {transaction: trx} )
             else
-                db.UserDetail.create({ ...userDetail, imageId: image_id}, { transaction: trx })
+                db.UserDetail.create({ userDetail }, { transaction: trx })
 
-            const result = await db.UserDetail.findOne({ where: { user_id } })
-            return result.toJSON()
+            const result = await db.UserDetail.findOne({ where: { userId } })
+            return {
+                userDetail: result
+            }
         } catch (err) {
             const customErr = parseError(err)
             if(customErr){
--- graphql/resolvers/user/userDetail/userDetail.query.js
+++ graphql/resolvers/user/userDetail/userDetail.query.js
@@ -2,15 +2,17 @@ const db = require('./../../../../model/index')
 const { regular } = require('./../../../../utils/helpers/middleware')
 const Sequelize = require('sequelize');
 
-const getUserDetail = async(_, { user_id }, { req }, info) => {
+const getUserDetail = async(_, { userId }, { req }, info) => {
     regular(req)
 
     try {
-        const result = await db.UserDetail.findOne({ where: {user_id} })
+        const result = await db.UserDetail.findOne({ where: { userId } })
+
         return {
             userDetail: result
         }
     } catch (err) {
+        console.log("hata\n")
         throw err
     }
 }
